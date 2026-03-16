c811c6e7d174a3482e422dae95f54437c675d6fd
aditya <aditya@gmail.com>
2026-03-16 13:54:05 +0530

    footer section done

---
+++ first.js
@@ -0,0 +1,6 @@
+export const add =(a,b) =>{
+    return a+b;
+}
+export const mul = (c,d) =>{
+    return c*d;
+}
---
+++ second.js
@@ -0,0 +1,7 @@
+import { add,mul } from "./first.js";
+
+let res = add(7,8);
+console.log(res);
+
+let c = mul(7,4);
+console.log(c)
