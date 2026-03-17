d45bd1211a4f253c8e477dd0dac4f28a61796e01
aditya <aditya@gmail.com>
2026-03-17 13:47:51 +0530

    jgjg

--- first.js
+++ first.js
@@ -4,3 +4,16 @@ export const add =(a,b) =>{
 export const mul = (c,d) =>{
     return c*d;
 }
+
+export const name = "subani";
+export const age = 76;
+export const city = "shamli";
+
+export const studentData = () =>{
+    return `name is ${name} his age is ${age} and his city is ${city}`
+}
+
+
+export const generateRandomNumber = () => {
+    
+}
--- second.js
+++ second.js
@@ -1,7 +1,10 @@
-import { add,mul } from "./first.js";
+import { add,mul,studentData } from "./first.js";
 
 let res = add(7,8);
 console.log(res);
 
 let c = mul(7,4);
 console.log(c)
+
+let d = studentData();
+console.log(d);
